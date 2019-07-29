7be63662853dc1a8a8f3473c75f10852950762a1
Michael McGee <michael@mcgee.io>
2019-07-29 21:36:02 +0700

    Made fix to bubble_sort_by method

---
+++ bubble_sort.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+def bubble_sort(arr)
+  sorted = false
+  unsorted_length = arr.size
+
+  until sorted
+    sorted = true
+    unsorted_length -= 1
+
+    unsorted_length.times do |i|
+      next if arr[i] <= arr[i + 1]
+
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      sorted = false
+    end
+  end
+
+  arr
+end
+
+def bubble_sort_by(arr)
+  sorted = false
+  unsorted_length = arr.size
+
+  until sorted
+    sorted = true
+    unsorted_length -= 1
+
+    unsorted_length.times do |i|
+      result = yield([arr[i], arr[i + 1]])
+
+      next if result.negative?
+
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      sorted = false
+    end
+  end
+
+  arr
+end
+
+result = bubble_sort([4, 3, 78, 2, 0, 2])
+p result
+
+result = bubble_sort_by(%w[hi hello hey howyadoin yo howdy]) do |left, right|
+  left.length - right.length
+end
+
+p result
